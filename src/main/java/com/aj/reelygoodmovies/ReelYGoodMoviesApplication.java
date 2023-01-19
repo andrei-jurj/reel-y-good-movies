4da8b16176aa847bd693da79cc9e41a1aadcfa47
Andrei Jurj <andreixjurj@gmail.com>
2023-01-19 12:49:22 +0200

    structure

---
+++ src/main/java/com/aj/reelygoodmovies/ReelYGoodMoviesApplication.java
@@ -0,0 +1,12 @@
+package com.aj.reelygoodmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReelYGoodMoviesApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReelYGoodMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aj/reelygoodmovies/ReelYGoodMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aj.reelygoodmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReelYGoodMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
